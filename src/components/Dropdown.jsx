3d3d92be75c41c8d2bb73092cde90b6c3781f7ed
ixouu <julienchanon@gmail.com>
2022-10-05 15:57:15 +0200

    dropdown logic has been updated

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Dropdown = ({ props, title }) => {
-    console.log(props);
-    // console.log(title);
+
+    const [isDown, setIsDown] = useState(false)
+    
+    const handleClick = () => {
+        isDown ? setIsDown(false) : setIsDown(true)
+    }
+    const displayProps = () => {
+        if(typeof props === "object"){
+            return (
+                <ul>
+                    {props.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            )
+        }else{
+            return <p>{props}</p>
+        }
+    }
+
     return (
-        <div>
-            Dropdown
+        <div className='dropdown'>
+            <div className="dropdown-head">
+                <h3>{title}</h3>
+                <button 
+                    type='button'
+                    onClick={() => handleClick()}
+                    ><i class="fa-solid fa-chevron-down"></i>
+                </button>
+            </div>
+            {isDown && 
+                <div className='dropdown-content'>
+                    {displayProps()}
+                </div>
+            }
         </div>
     );
 }
