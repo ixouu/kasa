769e2710fb53863b2a6a36ab5704cc58d7009ac0
ixouu <julienchanon@gmail.com>
2022-10-05 17:19:23 +0200

    rating logic has been added

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { startTransition } from 'react';
 
-const Rating = () => {
+const Rating = ({ rating }) => {
+    
+    const star = <i style={{color: '#FF6060'}} className="fa-solid fa-star"></i>
+    const emptyStar = <i style={{color: '#E3E3E3'}}className="fa-solid fa-star"></i>
+
+    const parsedRating = parseInt(rating);
+    const EmptyStarNumber = 5-parsedRating;
+    const stars = [];
+
+    for (let i = 0; i<parsedRating; i++){
+        stars.push(star)
+    }
+    for (let i = 0; i<EmptyStarNumber; i++){
+        stars.push(emptyStar)
+    }
     return (
-        <div>
-            RATING
-        </div>
+        <>
+            {
+               stars.map(e => {return <span>{e}</span>})
+            }
+        </>
     );
 }
 
