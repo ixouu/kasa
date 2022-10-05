062158d1f5558f7db33130402d5190fa15dacfc1
ixouu <julienchanon@gmail.com>
2022-10-05 18:29:11 +0200

    caroussel logic has been added

--- src/components/Caroussel.jsx
+++ src/components/Caroussel.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Caroussel = ({ images }) => {
 
-    console.log(images)
+    const [picNumber, setPicNumber] = useState(1);
+    const [picData, setPicData] = useState([]);
+    const [dataLength, setDataLength] = useState(0);
+
+    useEffect(() => {
+        setPicData(images);
+        setDataLength(images.length);
+        ;
+    },[]);
+
+    useEffect(() =>{
+        setTimeout(() => {
+            if (picNumber < dataLength){
+                setPicNumber(picNumber+1)
+            } else {
+                setPicNumber(1)
+            }
+        },5000)
+    },[picNumber, dataLength])
+    
+    const prevPic = () => {
+        if( picNumber !== 1){
+            setPicNumber(picNumber-1)
+        } else if (picNumber === 1){
+            setPicNumber(dataLength)
+        }
+    }
+    
+    const nextPic = () => {
+        if (picNumber < dataLength){
+            setPicNumber(picNumber+1)
+        } else {
+            setPicNumber(1)
+        }
+    }
+
     return (
         <div className='caroussel'>
-            {
-                images.map(i =>  <img src={i} alt='Previsualisation du logement'></img>)
-            }
+            <div className='caroussel-left'onClick={()=> prevPic()}><i className="fa-solid fa-chevron-left"></i></div>
+            <div className='caroussel-right' onClick={() => nextPic()}><i className="fa-solid fa-chevron-right"></i></div>
+            <img src={`${picData[picNumber-1]}`} alt=""/>
+            <div className='caroussel-count'>
+                <span>{picNumber}/{dataLength}</span>
+            </div>
         </div>
     );
 }
